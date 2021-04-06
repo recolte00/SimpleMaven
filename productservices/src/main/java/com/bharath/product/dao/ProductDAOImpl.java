da95d209a4732111ee279d211458ee86aa61e965
recolte00 <yukiyako2.lulu1920@gmail.com>
2021-04-07 00:11:50 +0900

    first commit

---
+++ productservices/src/main/java/com/bharath/product/dao/ProductDAOImpl.java
@@ -0,0 +1,31 @@
+package com.bharath.product.dao;
+
+import com.bharath.product.dto.Product;
+
+public class ProductDAOImpl implements ProductDAO {
+
+	@Override
+	public void create(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Product read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
